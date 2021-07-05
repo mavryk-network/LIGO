42378aa46b57500c59bb647a9b9a6c5cf7bcf883
Sander Spies <sandermail@gmail.com>
2021-07-05 14:28:49 +0200

    Clean up.

--- tools/ligo-syntax-highlighting/LigoSyntaxHighlighting.ml
+++ tools/ligo-syntax-highlighting/LigoSyntaxHighlighting.ml
@@ -6,51 +6,36 @@ let output_directory =
   let doc = "Output files at given directory." in
   Arg.(required & pos ~rev:true 0 (some string) None & info [] ~doc)
 
-let generate_cameligo_syntax_highlighting output_directory = 
+let generate_syntax_highlighting output_directory func file = 
   let open SyntaxHighlighting.Textmate in
-  match CameLIGO.syntax_highlighting () with 
+  match func () with 
     Ok s -> 
-      let fmt = Format.formatter_of_out_channel (open_out @@ Filename.concat output_directory "mligo.tmLanguage.json") in
-      Format.fprintf fmt "%s" s
-  | Error Referenced_rule_does_not_exist s -> Format.printf "Referenced rule '%s' does not exist." s
-  | Error Not_a_valid_reference s -> Format.printf "Not a valid reference: '%s'." s
-  | Error Meta_name_some_but_empty s -> Format.printf "%s.name has no value, but is expected to." s
-  | Error Begin_cant_be_empty s -> Format.printf "%s.begin_ can't be empty." s
-  | Error End_cant_be_empty s -> Format.printf "%s.end_ can't be empty" s
-
-let generate_pascaligo_syntax_highlighting output_directory = 
-  let open SyntaxHighlighting.Textmate in
-  match PascaLIGO.syntax_highlighting () with 
-    Ok s -> 
-      let fmt = Format.formatter_of_out_channel (open_out @@ Filename.concat output_directory "ligo.tmLanguage.json") in
-      Format.fprintf fmt "%s" s
-  | Error Referenced_rule_does_not_exist s -> Format.printf "Referenced rule '%s' does not exist." s
-  | Error Not_a_valid_reference s -> Format.printf "Not a valid reference: '%s'." s
-  | Error Meta_name_some_but_empty s -> Format.printf "%s.name has no value, but is expected to." s
-  | Error Begin_cant_be_empty s -> Format.printf "%s.begin_ can't be empty." s
-  | Error End_cant_be_empty s -> Format.printf "%s.end_ can't be empty" s
-
-let generate_reasonligo_syntax_highlighting output_directory = 
-  let open SyntaxHighlighting.Textmate in
-  match ReasonLIGO.syntax_highlighting () with 
-    Ok s -> 
-      let fmt = Format.formatter_of_out_channel (open_out @@ Filename.concat output_directory "religo.tmLanguage.json") in
-      Format.fprintf fmt "%s" s
-  | Error Referenced_rule_does_not_exist s -> Format.printf "Referenced rule '%s' does not exist." s
-  | Error Not_a_valid_reference s -> Format.printf "Not a valid reference: '%s'." s
-  | Error Meta_name_some_but_empty s -> Format.printf "%s.name has no value, but is expected to." s
-  | Error Begin_cant_be_empty s -> Format.printf "%s.begin_ can't be empty." s
-  | Error End_cant_be_empty s -> Format.printf "%s.end_ can't be empty" s
-
+      let fmt = Format.formatter_of_out_channel (open_out @@ Filename.concat output_directory file) in
+      Format.fprintf fmt "%s" s;
+      `Ok
+  | Error Referenced_rule_does_not_exist s -> `Error (false, Format.sprintf "Referenced rule '%s' does not exist." s)
+  | Error Not_a_valid_reference s -> `Error (false, Format.sprintf "Not a valid reference: '%s'." s)
+  | Error Meta_name_some_but_empty s -> `Error (false, Format.sprintf  "%s.name has no value, but is expected to." s)
+  | Error Begin_cant_be_empty s -> `Error (false, Format.sprintf  "%s.begin_ can't be empty." s)
+  | Error End_cant_be_empty s -> `Error (false, Format.sprintf  "%s.end_ can't be empty" s)
 
 let output output_directory = 
   if not (Sys.is_directory output_directory) then 
     `Error (false, "Not a valid directory")
   else (
-    generate_cameligo_syntax_highlighting output_directory;
-    generate_pascaligo_syntax_highlighting output_directory;
-    generate_reasonligo_syntax_highlighting output_directory;
-    `Ok "Generated syntax highlighting successful")
+    let cameligo_syntax_highlighting = generate_syntax_highlighting output_directory CameLIGO.syntax_highlighting "mligo.tmLanguage.json" in
+    match cameligo_syntax_highlighting with 
+      `Error e -> `Error e
+    | `Ok ->
+      let pascaligo_syntax_highlighting = generate_syntax_highlighting output_directory PascaLIGO.syntax_highlighting "ligo.tmLanguage.json" in
+      match pascaligo_syntax_highlighting with 
+        `Error e -> `Error e
+      | `Ok ->
+        let reasonligo_syntax_highlighting = generate_syntax_highlighting output_directory ReasonLIGO.syntax_highlighting "religo.tmLanguage.json" in
+        match reasonligo_syntax_highlighting with 
+          `Error e -> `Error e
+        | `Ok ->
+            `Ok "Generated syntax highlighting successful")
 
 let generate_syntax_highlighting = 
   let doc = "generate syntax highlighting" in
