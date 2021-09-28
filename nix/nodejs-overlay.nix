0e2c6e9b1b11b013a2ab8a3ec983423591c87a24
Michael Weichert <mweichert@gmail.com>
2021-09-28 19:51:12 +0000

    Attempt to use nodev16 for building

--- nix/nodejs-overlay.nix
+++ nix/nodejs-overlay.nix
@@ -1,6 +1,6 @@
 self: super: {
   # Note: this overlay doesn't apply to nix-npm-buildpackage
-  nodejs = super.nodejs-16_x;
-  nodePackages = super.nodePackages_16_x;
-  nodejs-slim = super.nodejs-slim-16_x;
+  nodejs = nodejs-16_x;
+  nodePackages = nodePackages_16_x;
+  nodejs-slim = nodejs-slim-16_x;
 }
