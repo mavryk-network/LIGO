2f6d89e3377c4368a9d7ef1b8f75bc90c35d9e11
Sander Spies <sandermail@gmail.com>
2021-10-25 20:56:28 +0200

    wip

--- src/passes/03-self_cst/reasonligo/self_cst_reasonligo.ml
+++ src/passes/03-self_cst/reasonligo/self_cst_reasonligo.ml
@@ -1,15 +1,17 @@
 module Errors = Errors
 
-let all_mapper ~raise = [
-  Helpers.fold_to_map () @@ Scoping.peephole ~raise
-]
+let all_mapper ~raise ~comments = 
+  ignore comments;
+  [
+    Helpers.fold_to_map () @@ Scoping.peephole ~raise
+  ]
 
-let all_module ~raise (init, _comments) = (* TODO: pass comments *)
-  let all_p = List.map ~f:Helpers.map_module @@ all_mapper ~raise in
+let all_module ~raise (init, comments) =
+  let all_p = List.map ~f:Helpers.map_module @@ all_mapper ~raise ~comments in
   List.fold ~f:(|>) all_p ~init
 
 let all_expression ~raise init =
-  let all_p = List.map ~f:Helpers.map_expression @@ all_mapper ~raise in
+  let all_p = List.map ~f:Helpers.map_expression @@ all_mapper ~raise ~comments:[] in
   List.fold ~f:(|>) all_p ~init
 
 let fold_expression = Helpers.fold_expression
