180cfa2219c33e75c468b794adaf967054ed6031
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2021-11-03 12:29:53 -0300

    Use Longident.parse

--- src/stages/common/ppx_mod_po.ml
+++ src/stages/common/ppx_mod_po.ml
@@ -15,7 +15,7 @@ let post_t =
       | _ -> false in
     List.exists ~f:aux xs in
   let gen_it ~loc ~name =
-    let t = ptyp_constr ~loc { txt = Ldot (lident name, "t") ; loc } [] in
+    let t = ptyp_constr ~loc { txt = Longident.parse (name ^ ".t") ; loc } [] in
     let type_declaration = type_declaration ~loc ~name:{ txt = String.lowercase_ascii name ; loc } ~params:[] ~cstrs:[] ~kind:Ptype_abstract ~private_:Public ~manifest:(Some t) in
     let new_desc = Pstr_type (Nonrecursive, [type_declaration]) in
     [ { pstr_desc = new_desc ; pstr_loc = loc } ] in
@@ -33,7 +33,7 @@ let post_pp =
     List.exists ~f:aux xs in
   let gen_it ~loc ~name =
     let value_binding = value_binding ~loc ~pat:(ppat_var ~loc @@ { txt = "pp_" ^ (String.lowercase_ascii name) ; loc })
-                          ~expr:(pexp_ident ~loc { txt = Ldot (lident name, "pp") ; loc }) in
+                          ~expr:(pexp_ident ~loc { txt = Longident.parse (name ^ ".pp") ; loc }) in
     let new_desc = Pstr_value (Nonrecursive, [value_binding]) in
     [ { pstr_desc = new_desc ; pstr_loc = loc } ] in
   { has_it ; gen_it }
@@ -48,13 +48,16 @@ let map_exprs = object
         Pstr_module { pmb_name = { txt = Some ml } ; pmb_expr = { pmod_desc = Pmod_structure sis } ; pmb_attributes } as pstr_desc ->
          let pstr_desc = super#structure_item_desc pstr_desc in
          let attrs = List.map ~f:(fun {attr_name;_} -> attr_name.txt) pmb_attributes in
-         let b = List.mem attrs "mod_po" ~equal:String.equal in
-         let aux r { has_it ; gen_it } =
-           if b || has_it sis then
-             r @ gen_it ~loc:pstr_loc ~name:ml
-           else
-             r in
-         List.fold ~init:[{ pstr_desc ; pstr_loc }] ~f:aux posts
+         if List.mem attrs "no_mod_po" ~equal:String.equal then
+           [{ pstr_desc ; pstr_loc }]
+         else
+           let b = List.mem attrs "force_mod_po" ~equal:String.equal in
+           let aux r { has_it ; gen_it } =
+             if b || has_it sis then
+               r @ gen_it ~loc:pstr_loc ~name:ml
+             else
+               r in
+           List.fold ~init:[{ pstr_desc ; pstr_loc }] ~f:aux posts
       | pstr_desc ->
          let pstr_desc = super#structure_item_desc pstr_desc in
          [{ pstr_desc ; pstr_loc }] in
