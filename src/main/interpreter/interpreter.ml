6557977ad7700246c396be0374d0b599ae8bc89f
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-02-01 09:35:39 -0300

    Remove C_TEST_EXTERNAL_CALL_TO_CONTRACT_EXN

--- src/main/interpreter/interpreter.ml
+++ src/main/interpreter/interpreter.ml
@@ -882,16 +882,6 @@ let rec apply_operator ~raise ~steps ~protocol_version ~options : Location.t -> 
        let>> addr  = Inject_script (loc, calltrace, code, storage, amt) in
        return @@ V_Record (LMap.of_list [ (Label "0", addr) ; (Label "1", code) ; (Label "2", size) ])
     | ( C_TEST_ORIGINATE , _  ) -> fail @@ error_type
-    | ( C_TEST_EXTERNAL_CALL_TO_CONTRACT_EXN , [ (V_Ct (C_contract contract)) ; param ; V_Ct ( C_mutez amt ) ] ) -> (
-       let* param_ty = monad_option (Errors.generic_error loc "Could not recover types") @@ List.nth types 1 in
-       let>> param = Eval (loc, param, param_ty) in
-       match param with
-       | V_Michelson (Ty_code { code = param ; _ }) ->
-          let>> res = External_call (loc,calltrace,contract,param,amt) in
-          return_contract_exec_exn res
-       | _ -> fail @@ Errors.generic_error loc "Error typing param"
-    )
-    | ( C_TEST_EXTERNAL_CALL_TO_CONTRACT_EXN , _  ) -> fail @@ error_type
     | ( C_TEST_EXTERNAL_CALL_TO_CONTRACT , [ (V_Ct (C_contract contract)) ; param; V_Ct ( C_mutez amt ) ] ) -> (
        let* param_ty = monad_option (Errors.generic_error loc "Could not recover types") @@ List.nth types 1 in
        let>> param = Eval (loc, param, param_ty) in
