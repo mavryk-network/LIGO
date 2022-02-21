b7d859277c1093cba77661be7d7089f27bd0ba13
Sander Spies <sandermail@gmail.com>
2022-02-21 19:36:57 +0100

    WASM backend: remove emscripten from build script WASM backend: use different gmp implementation

--- vendors/wasi/build.sh
+++ vendors/wasi/build.sh
@@ -1,7 +1,3 @@
-rm -rf gmp-6.2.1
-rm -rf wasi
-git clone https://github.com/torquem-ch/gmp-wasm gmp-6.2.1
-
 export WASI_VERSION=14
 export WASI_VERSION_FULL=${WASI_VERSION}.0
 
@@ -13,17 +9,3 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     tar xvf wasi-sdk-${WASI_VERSION_FULL}-macos.tar.gz
 fi
 
-WASI_SDK_PATH=wasi/wasi-sdk-14.0
-
-cd gmp-6.2.1
-make clean
-rm -rf dist
-mkdir dist
-./configure --prefix ${PWD}/dist CC=${PWD}/../wasi-sdk-14.0/bin/clang RANLIB=${PWD}/../wasi-sdk-14.0/bin/llvm-ranlib AR=${PWD}/../wasi-sdk-14.0/bin/llvm-ar --disable-assembly --host none
-make 
-make check
-make install
-
-cp dist/lib/libgmp.a ../
-cd ..
-ls -l libgmp.a
