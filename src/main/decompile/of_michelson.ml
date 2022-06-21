a0943235e3a1554a7d0ecfaddd66075486308853
Nicolas Phan <nicolas.van.phan@gmail.com>
2022-06-21 19:08:16 +0200

    [DONT MERGE] DEBUG PRINT on decompile_expression

--- src/main/decompile/of_michelson.ml
+++ src/main/decompile/of_michelson.ml
@@ -3,6 +3,7 @@ module Formatter = Formatter
 open Main_errors
 open Simple_utils.Trace
 open Simple_utils.Runned_result
+open Tezos_utils
 
 let decompile_value ~raise (output_type:Ast_aggregated.type_expression) (ty, value) =
   let mini_c     = trace ~raise main_decompile_michelson @@ Stacking.Decompiler.decompile_value ty value in
@@ -26,5 +27,13 @@ let decompile_expression ~raise (type_value: Ast_aggregated.type_expression) run
   match runned_result with
   | Fail s -> Fail s
   | Success ex_ty_value ->
+    let () = Format.printf "Toto\n" in
+    let ty, value = ex_ty_value in
+    let () = Michelson.pp_json Format.err_formatter ty in
+    let () = Michelson.pp_json Format.err_formatter value in
+    (* let open Tezos_utils.Micheline in *)
+    (* let a, b = ex_ty_value in *)
+    (* let convert_locations node = Micheline.map_node (fun _ -> {Micheline_printer.comment = None}) (fun x -> x) node in *)
+    (* let () = Format.printf "----> %a \n" Micheline_printer.print_expr (convert_locations ex_ty_value) in *)
     let decompiled_value = decompile_value ~raise type_value ex_ty_value in
     (Success decompiled_value)
