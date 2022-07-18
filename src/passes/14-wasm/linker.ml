4e5f86cd17c14e8231a01dca213415b1d3acb280
Sander Spies <sandermail@gmail.com>
2022-07-18 14:38:22 +0200

    WASM backend: try to fix build

--- src/passes/14-wasm/linker.ml
+++ src/passes/14-wasm/linker.ml
@@ -1,7 +1,7 @@
 let link files output =
   let command =
     sprintf
-      "./vendors/wasi/wasi-sdk-14.0/bin/wasm-ld -m wasm32 \
+      "wasm-ld -m wasm32 \
        -L${PWD}/vendors/wasi/wasi-sdk-14.0/share/wasi-sysroot/lib/wasm32-wasi \
        %s -lc \
        ${PWD}/vendors/wasi/wasi-sdk-14.0/lib/clang/13.0.0/lib/wasi/libclang_rt.builtins-wasm32.a \
