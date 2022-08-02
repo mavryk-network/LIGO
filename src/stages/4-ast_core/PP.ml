02db721c5141c0ea50a4e5db10f6b9a63ba39458
Alistair O'Brien <alistair.obrien@trili.tech>
2022-08-02 14:51:08 +0100

    Add `PP` and `To_yojson` implementations for `Ast_core`

--- src/stages/4-ast_core/PP.ml
+++ src/stages/4-ast_core/PP.ml
@@ -1,23 +1,31 @@
 [@@@coverage exclude_file]
+
 module Int64 = Caml.Int64
 open Types
-(* 
 open Format
 open Simple_utils.PP_helpers
-include Stage_common.PP *)
+include Stage_common.PP
 
 type 'a pretty_printer = Format.formatter -> 'a -> unit
-(* 
+
 let lmap_sep value sep ppf m =
-  let lst = List.sort ~compare:(fun (Label a,_) (Label b,_) -> String.compare a b) m in
+  let lst =
+    List.sort ~compare:(fun (Label a, _) (Label b, _) -> String.compare a b) m
+  in
   let new_pp ppf (k, v) = fprintf ppf "@[<h>%a -> %a@]" label k value v in
   fprintf ppf "%a" (list_sep new_pp sep) lst
+
 
 let record_sep value sep ppf (m : 'a label_map) =
   let lst = LMap.to_kv_list m in
-  let lst = List.dedup_and_sort ~compare:(fun (Label a,_) (Label b,_) -> String.compare a b) lst in
+  let lst =
+    List.dedup_and_sort
+      ~compare:(fun (Label a, _) (Label b, _) -> String.compare a b)
+      lst
+  in
   let new_pp ppf (k, v) = fprintf ppf "@[<h>%a -> %a@]" label k value v in
   fprintf ppf "%a" (list_sep new_pp sep) lst
+
 
 let tuple_sep value sep ppf m =
   assert (Helpers.is_tuple_lmap m);
@@ -25,20 +33,39 @@ let tuple_sep value sep ppf m =
   let new_pp ppf (_, v) = fprintf ppf "%a" value v in
   fprintf ppf "%a" (list_sep new_pp sep) lst
 
-let tuple_or_record_sep_t value format_record sep_record format_tuple sep_tuple ppf m =
-  if Helpers.is_tuple_lmap m then
-    fprintf ppf format_tuple (tuple_sep value (tag sep_tuple)) m
-  else
-    fprintf ppf format_record (record_sep value (tag sep_record)) m
 
-let tuple_or_record_sep_type value = tuple_or_record_sep_t value "@[<h>record[%a]@]" " ,@ " "@[<h>( %a )@]" " *@ "
+let tuple_or_record_sep_t
+  value
+  format_record
+  sep_record
+  format_tuple
+  sep_tuple
+  ppf
+  m
+  =
+  if Helpers.is_tuple_lmap m
+  then fprintf ppf format_tuple (tuple_sep value (tag sep_tuple)) m
+  else fprintf ppf format_record (record_sep value (tag sep_record)) m
+
+
+let tuple_or_record_sep_type value =
+  tuple_or_record_sep_t value "@[<h>record[%a]@]" " ,@ " "@[<h>( %a )@]" " *@ "
+
 
 let list_sep_d_short x = list_sep x (tag " , ")
 let list_sep_d x = list_sep x (tag " ,@ ")
-let kv_short value_pp ~assoc ppf (k, v) = fprintf ppf "%a%s%a" label k assoc value_pp v
+
+let kv_short value_pp ~assoc ppf (k, v) =
+  fprintf ppf "%a%s%a" label k assoc value_pp v
+
+
 let lmap_sep_short x ~sep ~assoc ppf m =
-  let lst = List.sort ~compare:(fun (Label a,_) (Label b,_) -> String.compare a b) m in
+  let lst =
+    List.sort ~compare:(fun (Label a, _) (Label b, _) -> String.compare a b) m
+  in
   list_sep (kv_short x ~assoc) (tag sep) ppf lst
+
+
 let lmap_sep_d x = lmap_sep x (tag " ,@ ")
 
 let rec constraint_identifier_unicode (ci : Int64.t) =
@@ -46,8 +73,8 @@ let rec constraint_identifier_unicode (ci : Int64.t) =
     let ( - ) = Int64.sub in
     let ( / ) = Int64.div in
     let ( * ) = Int64.mul in
-    match (ci - ((ci / 10L) * 10L)) with
-      a when Int64.equal a 0L -> "₀"
+    match ci - (ci / 10L * 10L) with
+    | a when Int64.equal a 0L -> "₀"
     | a when Int64.equal a 1L -> "₁"
     | a when Int64.equal a 2L -> "₂"
     | a when Int64.equal a 3L -> "₃"
@@ -57,14 +84,22 @@ let rec constraint_identifier_unicode (ci : Int64.t) =
     | a when Int64.equal a 7L -> "₇"
     | a when Int64.equal a 8L -> "₈"
     | a when Int64.equal a 9L -> "₉"
-    | _ -> failwith (Format.asprintf "internal error: couldn't pretty-print int64: %Li (is it a negative number?)" ci)
+    | _ ->
+      failwith
+        (Format.asprintf
+           "internal error: couldn't pretty-print int64: %Li (is it a negative \
+            number?)"
+           ci)
   in
-  if Int64.equal ci 0L then "" else (constraint_identifier_unicode (Int64.div ci 10L)) ^ digit
+  if Int64.equal ci 0L
+  then ""
+  else constraint_identifier_unicode (Int64.div ci 10L) ^ digit
 
-let t_attributes ppf ({ public ; hidden } : type_attribute) =
-  fprintf ppf "%a%a"
-    option_public public
-    option_hidden hidden
+
+let t_attributes ppf ({ public; hidden } : type_attribute) =
+  fprintf ppf "%a%a" option_public public option_hidden hidden
+
+
 let m_attributes ppf x = t_attributes ppf x
 
 let constraint_identifier_short ppf x =
@@ -72,78 +107,87 @@ let constraint_identifier_short ppf x =
   then Format.fprintf ppf "₀"
   else Format.fprintf ppf "%s" (constraint_identifier_unicode x)
 
+
 let list_sep_d_par f ppf lst =
   match lst with
   | [] -> ()
   | _ -> fprintf ppf " (%a)" (list_sep_d f) lst
 
-let rec type_expression ppf (te : type_expression) : unit =
-  (* TODO: we should have a way to hook custom pretty-printers for some types and/or track the "origin" of types as they flow through the constraint solver. This is a temporary quick fix *)
-  if Option.is_some (Combinators.get_t_bool   te) then bool   ppf    else 
-  if Option.is_some (Combinators.get_t_option te) then option ppf te 
-  else
-    fprintf ppf "%a" type_content te.type_content
+
+let rec term ppf (t : term) : unit =
+  (* TODO: we should have a way to hook custom pretty-printers for some 
+     types and/or track the "origin" of types as they flow through the 
+     constraint solver. This is a temporary quick fix 
+  *)
+  if Option.is_some (Combinators.get_t_bool t)
+  then bool ppf
+  else if Option.is_some (Combinators.get_t_option t)
+  then option ppf t
+  else fprintf ppf "%a" term_content t.term_content
+
+
 and bool ppf = fprintf ppf "%a" type_variable Stage_common.Constant.v_bool
-and option ppf (te : type_expression) = 
-  let t = Combinators.get_t_option te in
-    (match t with
-      Some t -> fprintf ppf "option (%a)" type_expression t
-    | None   -> fprintf ppf "option ('a)")
-and type_content : formatter -> type_content -> unit =
-  fun ppf te ->
-  match te with
-  | T_variable        tv -> type_variable ppf tv
-  | T_sum              m -> fprintf ppf "@[<h>sum[%a]@]" (lmap_sep_d row) (LMap.to_kv_list_rev m.fields)
-  | T_record           m -> fprintf ppf "%a" (tuple_or_record_sep_type row) m.fields
-  | T_arrow            a -> arrow         type_expression ppf a
-  | T_app              a -> type_app type_expression ppf a
-  | T_module_accessor ma -> module_access type_variable ppf ma
-  | T_singleton       x  -> literal       ppf             x
-  | T_abstraction     x  -> abstraction   type_expression ppf x
-  | T_for_all         x  -> for_all       type_expression ppf x
+
+and option ppf (t : term) =
+  match Combinators.get_t_option t with
+  | Some t -> fprintf ppf "option (%a)" term t
+  | None -> fprintf ppf "option ('a)"
+
+
+and term_content : term_content pretty_printer =
+ fun ppf t ->
+  match t with
+  | T_variable x -> term_variable ppf x
+  | T_sum { fields; _ } ->
+    fprintf ppf "@[<h>sum[%a]@]" (lmap_sep_d row) (LMap.to_kv_list_rev fields)
+  | T_prod { fields; _ } ->
+    fprintf ppf "%a" (tuple_or_record_sep_type row) fields
+  | T_arrow a -> arrow term ppf a
+  | T_module_accessor ma -> module_access term ppf ma
+  | T_literal l -> literal ppf l
+  | T_application app -> application term ppf app
+  | T_constructor c -> constructor term ppf c
+  | T_constant c -> constant term ppf c
+  | T_record r -> record term ppf r
+  | T_record_accessor ra -> record_accessor term ppf ra
+  | T_record_update ru -> record_update term ppf ru
+  | T_lambda l -> lambda term term ppf l
+  | T_recursive r -> recursive term term ppf r
+  | T_matching x -> fprintf ppf "%a" (match_exp term term) x
+  | T_let_in { let_binder; rhs; let_result; attr } ->
+    fprintf
+      ppf
+      "@[let %a =@;<1 2>%a%a in@ %a@]"
+      (binder term)
+      let_binder
+      term
+      rhs
+      e_attributes
+      attr
+      term
+      let_result
+  | T_mod_in { module_binder; rhs; let_result } ->
+    fprintf
+      ppf
+      "@[let module %a = struct@; @[<v>%a@] end in@ %a@]"
+      module_variable
+      module_binder
+      (module_expr term term e_attributes t_attributes m_attributes)
+      rhs
+      term
+      let_result
+  | T_raw_code r -> raw_code term ppf r
+  | T_ascription a -> ascription term term ppf a
+  | T_assign a -> assign term term ppf a
+  | T_type -> fprintf ppf "Type"
+  | T_pi p -> pi term ppf p
+
 
 and row : formatter -> row_element -> unit =
-  fun ppf { associated_type ; michelson_annotation=_ ; decl_pos=_ } ->
-    fprintf ppf "%a"
-      type_expression associated_type
+ fun ppf { associated_type; michelson_annotation = _; decl_pos = _ } ->
+  fprintf ppf "%a" term associated_type
 
-let rec expression ppf (e : expression) =
-  fprintf ppf "@[%a@]" expression_content e.expression_content
-and expression_content ppf (ec : expression_content) =
-  match ec with
-  | E_literal          l -> literal                    ppf l
-  | E_variable         n -> expression_variable        ppf n
-  | E_application      a -> application     expression ppf a
-  | E_constructor      c -> constructor     expression ppf c
-  | E_constant         c -> constant        expression ppf c
-  | E_record           r -> record          expression ppf r
-  | E_record_accessor ra -> record_accessor expression ppf ra
-  | E_record_update   ru -> record_update   expression ppf ru
-  | E_lambda    l -> lambda expression type_expression ppf l
-  | E_type_abstraction e -> type_abs expression ppf e
-  | E_recursive r -> recursive expression type_expression ppf r
-  | E_matching x -> fprintf ppf "%a" (match_exp expression type_expression) x
-  | E_let_in { let_binder ;rhs ; let_result; attr } ->
-    fprintf ppf "@[let %a =@;<1 2>%a%a in@ %a@]"
-      (binder type_expression) let_binder
-      expression rhs
-      e_attributes attr
-      expression let_result
-  | E_type_in   {type_binder; rhs; let_result} ->
-    fprintf ppf "@[let %a =@;<1 2>%a in@ %a@]"
-      type_variable type_binder
-      type_expression rhs
-      expression let_result
-  | E_mod_in {module_binder; rhs ; let_result;} ->
-    fprintf ppf "@[let module %a = struct@; @[<v>%a@] end in@ %a@]"
-      module_variable module_binder
-      (module_expr expression type_expression e_attributes t_attributes m_attributes) rhs
-      expression let_result
-  | E_raw_code r -> raw_code expression ppf r
-  | E_ascription a -> ascription expression type_expression ppf a
-  | E_module_accessor ma -> module_access expression_variable ppf ma
-  | E_assign a -> assign expression type_expression ppf a *)
 
-let module_ _ppf (_p : module_) = 
-  failwith "TODO: later"
-  (* declarations expression type_expression e_attributes t_attributes m_attributes ppf p *)
+
+let module_ _ppf (_p : module_) = failwith "TODO: later"
+(* declarations expression type_expression e_attributes t_attributes m_attributes ppf p *)
--- src/stages/4-ast_core/to_yojson.ml
+++ src/stages/4-ast_core/to_yojson.ml
@@ -1,222 +1,223 @@
 open Types
 open Stage_common.To_yojson
+
 type json = Yojson.Safe.t
 type 'a json_printer = 'a -> json
 
 let constant' = Stage_common.To_yojson.constant'
-
 let label = label_to_yojson
-let option f o =
-    match o with
-    | None   -> `List [ `String "None" ; `Null ]
-    | Some v -> `List [ `String "Some" ; f v ]
 
-let pair f g (x, y) = `Tuple [ f x ; g y ]
-let list f lst = `List (List.map ~f:f lst)
+let option f o =
+  match o with
+  | None -> `List [ `String "None"; `Null ]
+  | Some v -> `List [ `String "Some"; f v ]
+
+
+let pair f g (x, y) = `Tuple [ f x; g y ]
+let list f lst = `List (List.map ~f lst)
+
 let label_map f lmap =
-  let lst = List.sort ~compare:(fun (Label a, _) (Label b, _) -> String.compare a b) (LMap.bindings lmap) in
-  let lst' = List.fold_left
-    ~f:(fun acc (Label k, v) -> (k , f v)::acc)
-    ~init:[] lst
+  let lst =
+    List.sort
+      ~compare:(fun (Label a, _) (Label b, _) -> String.compare a b)
+      (LMap.bindings lmap)
+  in
+  let lst' =
+    List.fold_left ~f:(fun acc (Label k, v) -> (k, f v) :: acc) ~init:[] lst
   in
   `Assoc lst'
+
 
 let layout = function
   | L_comb -> `List [ `String "L_comb"; `Null ]
   | L_tree -> `List [ `String "L_tree"; `Null ]
 
 
-let rec type_expression _ =
-  failwith "TODO: Re-implement with ppx"
-  (* `Assoc [
-    ("type_content", type_content tc);
-    ("sugar", option Ast_sugar.Yojson.type_expression sugar);
-    ("location", Location.to_yojson location);
-  ] *)
-
-and type_content = 
-  failwith "TODO: Re-implement with ppx"
-  (* function
-  | T_variable        t -> `List [ `String "t_variable"; TypeVar.to_yojson t]
-  | T_sum             t -> `List [ `String "t_sum"; rows t]
-  | T_record          t -> `List [ `String "t_record"; rows t]
-  | T_arrow           t -> `List [ `String "t_arrow"; arrow t]
-  | T_app             t -> `List [ `String "t_app";      t_app type_expression t]
-  | T_module_accessor t -> `List [ `String "t_module_accessor"; module_access TypeVar.to_yojson t]
-  | T_singleton       t -> `List [ `String "t_singleton" ; literal t ]
-  | T_abstraction     t -> `List [ `String "t_abstraction" ; for_all type_expression t ]
-  | T_for_all         t -> `List [ `String "t_for_all" ; for_all type_expression t ] *)
+let sugar = function
+  | Type_expr type_expr ->
+    `List [ `String "Type_expr"; Ast_sugar.Yojson.type_expression type_expr ]
+  | Expr expr -> `List [ `String "Expr"; Ast_sugar.Yojson.expression expr ]
 
 
-and rows _ =
-  failwith "TODO: Re-implement with ppx"
-  (* `Assoc [
-    ("content", label_map row_element fields);
-    ("layout", option layout l);
-  ] *)
+let rec term { term_content = tc; location; sugar = s } =
+  `Assoc
+    [ "term_content", term_content tc
+    ; "location", Location.to_yojson location
+    ; "sugar", option sugar s
+    ]
 
-and row_element _ =
-  failwith "TODO: Re-implement with ppx"
-  (* `Assoc [
-    ("associated_type", type_expression associated_type);
-    ("michelson_annotation", option (fun s -> `String s) michelson_annotation);
-    ("decl_pos", `Int decl_pos);
-  ] *)
 
-and arrow {type1;type2} =
-  `Assoc [
-    ("type1", type_expression type1);
-    ("type2", type_expression type2);
-  ]
-
-let rec expression _ =
-  failwith "TODO: Re-implement with ppx"
-  (* `Assoc [
-    ("expression_content", expression_content ec);
-    ("location", Location.to_yojson location);
-    ("core", option Ast_sugar.Yojson.expression sugar);
-  ] *)
-
-and expression_content _ = 
-  failwith "TODO: Re-implement with ppx"
-(* function
+and term_content = function
   (* Base *)
-  | E_literal     e -> `List [ `String "E_literal"; Stage_common.To_yojson.literal e ]
-  | E_constant    e -> `List [ `String "E_constant"; constant e ]
-  | E_variable    e -> `List [ `String "E_variable"; ValueVar.to_yojson e ]
-  | E_application e -> `List [ `String "E_application"; application e ]
-  | E_lambda      e -> `List [ `String "E_lambda"; lambda e ]
-  | E_type_abstraction e -> `List [ `String "E_type_abstraction"; type_abs expression e ]
-  | E_recursive   e -> `List [ `String "E_recursive"; recursive e ]
-  | E_let_in      e -> `List [ `String "E_let_in"; let_in e ]
-  | E_type_in     e -> `List [ `String "E_type_in"; type_in e ]
-  | E_mod_in      e -> `List [ `String "E_mod_in"; mod_in e ]
-  | E_raw_code    e -> `List [ `String "E_raw_code"; raw_code e ]
+  | T_literal lit ->
+    `List [ `String "T_literal"; Stage_common.To_yojson.literal lit ]
+  | T_variable x -> `List [ `String "T_variable"; TermVar.to_yojson x ]
+  | T_constant const -> `List [ `String "T_constant"; constant const ]
+  | T_application app -> `List [ `String "T_application"; application app ]
+  | T_lambda lamb -> `List [ `String "T_lambda"; lambda lamb ]
+  | T_recursive rec_ -> `List [ `String "T_recursive"; recursive rec_ ]
+  | T_let_in l -> `List [ `String "T_let_in"; let_in l ]
+  | T_mod_in m -> `List [ `String "T_mod_in"; mod_in m ]
+  | T_raw_code code -> `List [ `String "T_raw_code"; raw_code code ]
   (* Variant *)
-  | E_constructor     e -> `List [ `String "E_constructor"; constructor expression e ]
-  | E_matching        e -> `List [ `String "E_matching"; match_exp expression type_expression e ]
+  | T_constructor constr ->
+    `List [ `String "T_constructor"; constructor term constr ]
+  | T_matching m -> `List [ `String "T_matching"; match_exp term term m ]
   (* Record *)
-  | E_record          e -> `List [ `String "E_record"; record e ]
-  | E_record_accessor e -> `List [ `String "E_record_accessor"; record_accessor e ]
-  | E_record_update   e -> `List [ `String "E_record_update"; record_update e ]
-  | E_module_accessor e -> `List [ `String "E_module_accessor"; module_access ValueVar.to_yojson e]
-  | E_ascription      e -> `List [ `String "E_module_accessor"; ascription expression type_expression e]
-  | E_assign          e -> `List [ `String "E_assign";   assign expression type_expression e ] *)
-
-and constant {cons_name;arguments} =
-  `Assoc [
-    ("cons_name", constant' cons_name);
-    ("arguments", list expression arguments);
-  ]
-
-and application {lamb;args} =
-  `Assoc [
-    ("lamb", expression lamb);
-    ("args", expression args);
-  ]
-
-and lambda {binder=b;output_type;result} =
-  `Assoc [
-    ("binder", binder type_expression b);
-    ("output_type", option type_expression output_type);
-    ("result", expression result);
-  ]
-
-and recursive {fun_name;fun_type;lambda=l} =
-  `Assoc [
-    ("fun_name", ValueVar.to_yojson fun_name);
-    ("fun_type", type_expression fun_type);
-    ("lambda", lambda l)
-  ]
-
-and let_in {let_binder;rhs;let_result;attr} =
-  `Assoc [
-    ("let_binder", binder type_expression let_binder);
-    ("rhs", expression rhs);
-    ("let_result", expression let_result);
-    ("attr", known_attribute attr);
-  ]
-
-and type_in {type_binder;rhs;let_result} =
-  `Assoc [
-    ("type_binder", TypeVar.to_yojson type_binder);
-    ("rhs", type_expression rhs);
-    ("let_result", expression let_result)
-  ]
+  | T_record rec_ -> `List [ `String "T_record"; record rec_ ]
+  | T_record_accessor rec_acc ->
+    `List [ `String "T_record_accessor"; record_accessor rec_acc ]
+  | T_record_update rec_upd ->
+    `List [ `String "T_record_update"; record_update rec_upd ]
+  | T_module_accessor mod_acc ->
+    `List [ `String "T_module_accessor"; module_access term mod_acc ]
+  | T_ascription asc ->
+    `List [ `String "T_module_accessor"; ascription term term asc ]
+  | T_assign ass -> `List [ `String "T_assign"; assign term term ass ]
+  | T_sum r -> `List [ `String "T_sum"; rows r ]
+  | T_prod r -> `List [ `String "T_record"; rows r ]
+  | T_arrow arr -> `List [ `String "T_arrow"; arrow arr ]
+  | T_type -> `List [ `String "T_type" ]
+  | T_pi p -> `List [ `String "T_pi"; pi term p ]
 
 
-and known_attribute {inline;no_mutation;public;view;hidden} =
-  `Assoc [
-    ("inline", `Bool inline);
-    ("no_mutation", `Bool no_mutation);
-    ("public", `Bool public);
-    ("view", `Bool view);
-    ("hidden", `Bool hidden);
-  ]
+and constant { cons_name; arguments } =
+  `Assoc [ "cons_name", constant' cons_name; "arguments", list term arguments ]
 
-and type_attribute _ =
-  failwith "TODO: Re-implement with ppx"
-  (* `Assoc [
-    ("public", `Bool public);
-    ("hidden", `Bool hidden);
-  ] *)
 
-and module_attribute _ =
-  failwith "TODO: Re-implement with ppx"
-  (* `Assoc [
-    ("public", `Bool public);
-    ("hidden", `Bool hidden);
-  ] *)
+and application { lamb; args } = `Assoc [ "lamb", term lamb; "args", term args ]
+
+and lambda { binder = b; output_type; result } =
+  `Assoc
+    [ "binder", binder term b
+    ; "output_type", option term output_type
+    ; "result", term result
+    ]
+
+
+and recursive { fun_name; fun_type; lambda = l } =
+  `Assoc
+    [ "fun_name", ValueVar.to_yojson fun_name
+    ; "fun_type", term fun_type
+    ; "lambda", lambda l
+    ]
+
+
+and let_in { let_binder; rhs; let_result; attr } =
+  `Assoc
+    [ "let_binder", binder term let_binder
+    ; "rhs", term rhs
+    ; "let_result", term let_result
+    ; "attr", known_attribute attr
+    ]
+
+
+and type_in { type_binder; rhs; let_result } =
+  `Assoc
+    [ "type_binder", TypeVar.to_yojson type_binder
+    ; "rhs", term rhs
+    ; "let_result", term let_result
+    ]
+
+
+and known_attribute { inline; no_mutation; public; view; hidden } =
+  `Assoc
+    [ "inline", `Bool inline
+    ; "no_mutation", `Bool no_mutation
+    ; "public", `Bool public
+    ; "view", `Bool view
+    ; "hidden", `Bool hidden
+    ]
+
+
+and rows { fields; layout = l } =
+  `Assoc [ "fields", label_map row_element fields; "layout", option layout l ]
+
+
+and row_element { associated_type; michelson_annotation; decl_pos } =
+  `Assoc
+    [ "associated_type", term associated_type
+    ; "michelson_annotation", option (fun s -> `String s) michelson_annotation
+    ; "decl_pos", `Int decl_pos
+    ]
+
+
+and arrow { type1; type2 } = `Assoc [ "type1", term type1; "type2", term type2 ]
+
+and type_attribute { public; hidden } =
+  `Assoc [ "public", `Bool public; "hidden", `Bool hidden ]
+
+
+and module_attribute { public; hidden } =
+  `Assoc [ "public", `Bool public; "hidden", `Bool hidden ]
 
 
 and mod_in m =
-  Stage_common.To_yojson.mod_in expression type_expression known_attribute type_attribute module_attribute m
+  Stage_common.To_yojson.mod_in
+    term
+    term
+    known_attribute
+    type_attribute
+    module_attribute
+    m
 
-and raw_code _ =
-  failwith "TODO: R"
-  (* `Assoc [
-    ("language", `String language);
-    ("code", expression code);
-  ] *)
 
-and constructor _ =
-  failwith "TODO: Re-implement with ppx"
-  (* `Assoc [
-    ("constructor", label constructor);
-    ("element", expression element);
-  ] *)
+and raw_code { language; code } =
+  `Assoc [ "language", `String language; "code", term code ]
 
-and matching x = match_exp expression type_expression x
 
-and record r = label_map expression r
+and constructor
+  term
+  ({ constructor; element } : _ Stage_common.Types.constructor)
+  : json
+  =
+  `Assoc [ "constructor", label constructor; "element", term element ]
 
-and record_accessor _ =
-  failwith "TODO: Re-implement with ppx"
-  (* `Assoc [
-    ("record", expression record);
-    ("path", label path);
-  ] *)
 
-and record_update _ =
-  failwith "TODO: Re-implement with ppx"
-  (* `Assoc [
-    ("record", expression record);
-    ("path", label path);
-    ("update", expression update);
-  ] *)
+and matching x = match_exp term term x
+and record r = label_map term r
+
+and record_accessor { record; path } =
+  `Assoc [ "record", term record; "path", label path ]
+
+
+and record_update { record; path; update } =
+  `Assoc [ "record", term record; "path", label path; "update", term update ]
 
 
 and declaration_type x =
-  Stage_common.To_yojson.declaration_type type_expression type_attribute x
+  Stage_common.To_yojson.declaration_type term type_attribute x
+
 
 and declaration_constant x =
-  Stage_common.To_yojson.declaration_constant expression type_expression known_attribute x
+  Stage_common.To_yojson.declaration_constant term term known_attribute x
+
 
 and declaration_module x =
-  Stage_common.To_yojson.declaration_module expression type_expression known_attribute type_attribute module_attribute x
+  Stage_common.To_yojson.declaration_module
+    term
+    term
+    known_attribute
+    type_attribute
+    module_attribute
+    x
+
 
 and declaration x =
-  Stage_common.To_yojson.declaration expression type_expression known_attribute type_attribute module_attribute x
+  Stage_common.To_yojson.declaration
+    term
+    term
+    known_attribute
+    type_attribute
+    module_attribute
+    x
+
 
 and declarations x =
-  Stage_common.To_yojson.declarations expression type_expression known_attribute type_attribute module_attribute x
+  Stage_common.To_yojson.declarations
+    term
+    term
+    known_attribute
+    type_attribute
+    module_attribute
+    x
