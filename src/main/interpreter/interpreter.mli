5ad182b29371ed1262bf6a2acf3a954f43a5ed25
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-10-01 16:47:27 -0300

    feat: add eval_expression

--- src/main/interpreter/interpreter.mli
+++ src/main/interpreter/interpreter.mli
@@ -6,3 +6,11 @@ val eval_test
   -> options:Compiler_options.t
   -> Ast_typed.program
   -> (string * Ligo_interpreter.Types.value) list
+
+val eval_expression
+  :  raise:(interpreter_error, Main_warnings.all) Simple_utils.Trace.raise
+  -> steps:int
+  -> options:Compiler_options.t
+  -> Ast_typed.program
+  -> Ast_typed.expression
+  -> Ligo_interpreter.Types.value
