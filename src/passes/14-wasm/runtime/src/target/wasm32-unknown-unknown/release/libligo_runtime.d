cb8f2ae9600f9c4f5ea88f630117c7162552bdd1
Sander Spies <sandermail@gmail.com>
2022-10-13 11:44:11 +0200

    Test if this fixes CI.

---
+++ src/passes/14-wasm/runtime/src/target/wasm32-unknown-unknown/release/libligo_runtime.d
@@ -0,0 +1,1 @@
+/Users/Sander/Projects/ligo/src/passes/14-wasm/runtime/src/target/wasm32-unknown-unknown/release/libligo_runtime.a: /Users/Sander/Projects/ligo/src/passes/14-wasm/runtime/src/runtime/src/lib.rs
