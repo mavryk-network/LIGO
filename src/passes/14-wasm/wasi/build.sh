a93621c33d492e59bc79dacc9c9767196b94f161
Sander Spies <sandermail@gmail.com>
2022-10-13 14:37:32 +0200

    Trying to fix CI.

--- src/passes/14-wasm/wasi/build.sh
+++ src/passes/14-wasm/wasi/build.sh
@@ -1,11 +1,9 @@
 export WASI_VERSION=14
 export WASI_VERSION_FULL=${WASI_VERSION}.0
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_VERSION}/wasi-sdk-${WASI_VERSION_FULL}-linux.tar.gz
+if [[ "$OSTYPE" == "linux-"* ]]; then
     tar xvf wasi-sdk-${WASI_VERSION_FULL}-linux.tar.gz
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_VERSION}/wasi-sdk-${WASI_VERSION_FULL}-macos.tar.gz
     tar xvf wasi-sdk-${WASI_VERSION_FULL}-macos.tar.gz
 fi
 
