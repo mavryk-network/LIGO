e86d4a7632f339890d203ed1010dd89ceb30fd7b
Nicolas Phan <nicolas.van.phan@gmail.com>
2022-10-20 14:38:17 +0200

    Cleanup - Leave filerting of Directives as is

--- src/passes/03-unification/shared/helpers.ml
+++ src/passes/03-unification/shared/helpers.ml
@@ -10,7 +10,6 @@ let w_split (x: 'a Lexing_shared.Wrap.t) : 'a * Location.t =
   (x#payload, Location.lift x#region)
 let w_fst x = fst (w_split x)
 
-(* TODO NP : Find a better way to remove [Directive _] in the declaration list *)
 let filter_opt : 'a . 'a option list -> 'a list = fun decls ->
   List.fold
     ~init:[]
