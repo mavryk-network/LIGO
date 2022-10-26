509ff8262f02b0ae72f58c78f4061f05130fd616
Melwyn Saldanha <melwyn95@gmail.com>
2022-10-27 04:58:42 +0530

    Run Pattern_simpl

--- src/passes/13.1-expand_pattern/expand_pattern.ml
+++ src/passes/13.1-expand_pattern/expand_pattern.ml
@@ -1,6 +1,9 @@
-let compile : Ast_aggregated.expression -> Ast_pattern_expanded.expression 
-= fun e ->
-(* let () = Format.eprintf "before compiling pattern -> %a" Ast_aggregated.PP.expression e in *)
-Compiler.compile_expression e
+let compile : Ast_aggregated.expression -> Ast_pattern_expanded.expression =
+ fun e ->
+  let e = Compiler.compile_expression e in
+  let e = Pattern_simpl.peephole_expression e in
+  e
 
-let decompile : Ast_pattern_expanded.expression -> Ast_aggregated.expression = Decompiler.decompile_expression
+
+let decompile : Ast_pattern_expanded.expression -> Ast_aggregated.expression =
+  Decompiler.decompile_expression
