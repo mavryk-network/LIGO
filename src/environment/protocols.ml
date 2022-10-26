1b3e74894ed426f5c7beaa9be796fb2e1090319b
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-10-26 10:28:08 -0300

    refactor: use ppx for deriving eq/compare on Protocols.t

--- src/environment/protocols.ml
+++ src/environment/protocols.ml
@@ -1,4 +1,4 @@
-type t = Kathmandu | Lima
+type t = Kathmandu | Lima [@@deriving eq, compare]
 let current = Kathmandu
 let in_use = Lima (* Protocol we depend on *)
 
@@ -6,23 +6,14 @@ let in_use = Lima (* Protocol we depend on *)
 let protocols_str : string list = [ "kathmandu" ; "lima" ]
 
 let protocols_to_variant : string -> t option = fun p ->
-	match p with
-	| "current" -> Some current
+  match p with
+  | "current" -> Some current
   | "lima" -> Some Lima
   | "kathmandu" -> Some Kathmandu
-	| i when not (List.exists ~f:(String.equal i) protocols_str) -> None
-	| _ -> failwith "internal error: forgot to add the protocol string form to the list ?"
+  | i when not (List.exists ~f:(String.equal i) protocols_str) -> None
+  | _ -> failwith "internal error: forgot to add the protocol string form to the list ?"
 
 let variant_to_string : t -> string = fun s ->
-	match s with
+  match s with
   | Lima -> "lima"
   | Kathmandu -> "kathmandu"
-
-let compare : t -> t -> int = fun p q ->
-  match p, q with
-  | Lima, Lima -> 0
-  | Kathmandu, Kathmandu -> 0
-  | Kathmandu, Lima -> -1
-  | Lima, Kathmandu -> 1
-
-let equal : t -> t -> bool = fun a b -> (compare a b = 0)
