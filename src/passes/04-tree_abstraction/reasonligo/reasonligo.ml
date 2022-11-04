022653b9193cd00e793199b80b03d6e34e70ad25
Remi Lesenechal <remi.lesenechal@marigold.dev>
2022-11-04 12:00:48 +0100

    aa

--- src/passes/04-tree_abstraction/reasonligo/reasonligo.ml
+++ src/passes/04-tree_abstraction/reasonligo/reasonligo.ml
@@ -8,6 +8,6 @@ let compile_program    = Compiler.compile_program
 let compile_expression = Compiler.compile_expression
 
 let decompile_program  _  = failwith "Nah"
-let decompile_expression = failwith "Nah"
+let decompile_expression _ = failwith "Nah"
 
 let decompile_pattern_to_string _ = failwith "Nah"
