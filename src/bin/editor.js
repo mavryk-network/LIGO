b3098200171b25a4985cedceb0af82e0fbf64a9e
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2022-12-19 23:53:31 +0530

    More js_main nonsense

---
+++ src/bin/editor.js
@@ -0,0 +1,68 @@
+import { EditorState } from "@codemirror/state";
+import { EditorView, basicSetup } from "codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
+let ligoEditor = new EditorView({
+  state: EditorState.create({
+    extensions: [basicSetup, javascript()],
+    doc: `
+type storage = int
+
+type parameter =
+  Increment of int
+| Decrement of int
+| Reset
+
+// Two entrypoints
+
+let add (store, delta : storage * int) = store + delta
+let sub (store, delta : storage * int) = store - delta
+
+(* Main access point that dispatches to the entrypoints according to
+   the smart contract parameter. *)
+
+let main (action, store : parameter * storage) : operation list * storage =
+ [],    // No operations
+ (match action with
+   Increment (n) -> add (store, n)
+ | Decrement (n) -> sub (store, n)
+ | Reset         -> 0)
+
+(* Tests for main access point *)
+
+let initial_storage = 42
+
+let test_initial_storage =
+ let (taddr, _, _) = Test.originate main initial_storage 0tez in
+ assert (Test.get_storage taddr = initial_storage)
+
+let test_increment =
+ let (taddr, _, _) = Test.originate main initial_storage 0tez in
+ let contr = Test.to_contract taddr in
+ let _ = Test.transfer_to_contract_exn contr (Increment 1) 1mutez in
+ assert (Test.get_storage taddr = initial_storage + 1)
+`,
+  }),
+  parent: document.getElementById("ligo"),
+});
+
+let michelsonEditor = new EditorView({
+  state: EditorState.create({
+    extensions: [basicSetup, javascript()],
+    doc: `
+`,
+  }),
+  parent: document.getElementById("michelson"),
+});
+
+document.getElementById("compile").addEventListener("click", function () {
+  let michelson = compile.main(ligoEditor.state.doc.toJSON().join("\n"));
+  console.log(michelson);
+  michelsonEditor.setState(
+    EditorState.create({
+      extensions: [basicSetup],
+      doc: michelson,
+    })
+  );
+  // console.log(compile.main());
+});
