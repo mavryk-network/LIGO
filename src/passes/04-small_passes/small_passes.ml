c3334229712afa2a13632891e4cd3a22eb80ebd9
Remi Lesenechal <remi.lesenechal@marigold.dev>
2023-01-18 00:35:24 +0100

    aesthetic

--- src/passes/04-small_passes/small_passes.ml
+++ src/passes/04-small_passes/small_passes.ml
@@ -3,29 +3,6 @@ module O = Ast_core
 open Passes.Pass_type
 module Errors = Passes.Errors
 
-let trivial_compile_program : I.program -> O.program =
- fun _ ->
-  failwith "TODO: Everything is fine"
-
-
-let trivial_compile_expression : I.expr -> O.expression =
- fun _ -> failwith "TODO : Everything is fine"
-
-
-let compile_with_passes : type a. a sub_pass list -> a -> a =
- fun passes prg ->
-  let f : a * a dyn_reduction_check list -> a sub_pass -> a * a dyn_reduction_check list =
-   fun (prg, checks) pass ->
-    let prg = pass.forward prg in
-    (* checking all the reductions so far *)
-    let checks = pass.forward_check :: checks in
-    (combine_checks checks) prg;
-    prg, checks
-  in
-  let prg, _ = List.fold passes ~init:(prg, []) ~f in
-  prg
-
-
 let passes
     ~(raise : (Passes.Errors.t, _) Simple_utils.Trace.raise)
     ~(syntax : Syntax_types.t)
@@ -54,11 +31,34 @@ let passes
   ]
 
 
+let trivial_compile_program : I.program -> O.program =
+ fun _ -> failwith "TODO: Everything is fine"
+
+
+let trivial_compile_expression : I.expr -> O.expression =
+ fun _ -> failwith "TODO : Everything is fine"
+
+
+let compile_with_passes : type a. a sub_pass list -> a -> a =
+ fun passes prg ->
+  let f : a * a dyn_reduction_check list -> a sub_pass -> a * a dyn_reduction_check list =
+   fun (prg, checks) pass ->
+    let prg = pass.forward prg in
+    (* checking all the reductions so far *)
+    let checks = pass.forward_check :: checks in
+    (combine_checks checks) prg;
+    prg, checks
+  in
+  let prg, _ = List.fold passes ~init:(prg, []) ~f in
+  prg
+
+
 let compile_program ~raise ~syntax : I.program -> O.program =
  fun prg ->
   let passes = passes ~raise ~syntax in
   let prg = compile_with_passes (List.map ~f:(fun x -> x.program) passes) prg in
-  print_endline (Format.asprintf "%a" (Sexp.pp_hum_indent 2) (I.S_exp.sexp_of_program prg));
+  print_endline
+    (Format.asprintf "%a" (Sexp.pp_hum_indent 2) (I.S_exp.sexp_of_program prg));
   trivial_compile_program prg
 
 
