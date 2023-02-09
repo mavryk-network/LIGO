0c104753099aeab247cac3bce918ebc53f474b58
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2023-02-09 18:38:47 +0530

    Abstraction.t tuplised

--- src/stages/ligo_primitive/abstraction.ml
+++ src/stages/ligo_primitive/abstraction.ml
@@ -3,8 +3,17 @@ type 'a t =
   ; kind : Kind.t
   ; type_ : 'a
   }
-[@@deriving eq, compare, yojson, hash, fold, map, sexp]
+[@@deriving eq, compare, hash, fold, map, sexp]
 (* Lambda (a : kind). term *)
+
+let to_yojson v_to_yojson { ty_binder; kind; type_ } = `List [ Var.Type_var.to_yojson ty_binder; Kind.to_yojson kind; v_to_yojson type_ ]
+let of_yojson v_of_yojson (yojson: Yojson.Safe.t) =
+  match yojson with 
+  | `List [ty_binder_yojson; kind_yojson; v_yojson ] ->
+     (match Var.Type_var.of_yojson ty_binder_yojson, Kind.of_yojson kind_yojson, v_of_yojson v_yojson with
+     | Ok ty_binder, Ok kind, Ok type_ -> Ok { ty_binder; kind; type_ }
+     | _ -> Error "Failed to parse abstract.t") 
+  | _ -> Error "Failed to parse abstract.t" 
 
 let pp_forall f ppf { ty_binder; kind; type_ } : unit =
   Format.fprintf ppf "∀ %a : %a . %a" Var.Type_var.pp ty_binder Kind.pp kind f type_
