6338f58cc411c7ebe7711c48cb91b5de57af71cb
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2023-02-28 19:42:56 +0100

    feat: provide hint on error

--- src/main/helpers/syntax.ml
+++ src/main/helpers/syntax.ml
@@ -3,13 +3,13 @@ open Trace
 open Main_errors
 open Syntax_types
 
-let file_extension_to_variant ~raise ~support_pascaligo sf : t option =
-  ignore raise;
+let file_extension_to_variant ~raise ~support_pascaligo sf : t =
   match sf with
-  | ".mligo" -> Some CameLIGO
-  | ".jsligo" -> Some JsLIGO
-  | (".ligo" | ".pligo") when support_pascaligo -> Some PascaLIGO
-  | _ -> None
+  | ".mligo" -> CameLIGO
+  | ".jsligo" -> JsLIGO
+  | (".ligo" | ".pligo") when support_pascaligo -> PascaLIGO
+  | (".ligo" | ".pligo") when not support_pascaligo -> raise.error (main_deprecated_pascaligo_filename sf)
+  | _ -> raise.error (main_invalid_extension sf)
 
 
 let of_ext_opt ~support_pascaligo = function
@@ -24,13 +24,11 @@ let of_string_opt ~raise ~support_pascaligo (Syntax_name syntax) source =
   match syntax, source with
   | "auto", Some sf ->
     let ext = Caml.Filename.extension sf in
-    trace_option
-      ~raise
-      (main_invalid_extension ext)
-      (file_extension_to_variant ~support_pascaligo ~raise ext)
+    file_extension_to_variant ~support_pascaligo ~raise ext
   | ("cameligo" | "CameLIGO"), _ -> CameLIGO
   | ("jsligo" | "JsLIGO"), _ -> JsLIGO
   | ("pascaligo" | "PascaLIGO"), _ when support_pascaligo -> PascaLIGO
+  | ("pascaligo" | "PascaLIGO"), _ when not support_pascaligo -> raise.error (main_deprecated_pascaligo_syntax ())
   | _ -> raise.error (main_invalid_syntax_name syntax)
 
 
