110526cb95f568f8a8516d2ac000620d901ad55e
Remi Lesenechal <remi.lesenechal@marigold.dev>
2023-04-21 13:26:28 +0200

    proof of concept

--- src/passes/04-nanopasses/passes/unit_test_helpers.ml
+++ src/passes/04-nanopasses/passes/unit_test_helpers.ml
@@ -1,4 +1,5 @@
 open Simple_utils.Trace
+open Simple_utils.Function
 open Ast_unified
 
 let try_with f in_ =
@@ -12,6 +13,37 @@ let try_with f in_ =
         "Err : %a"
         Errors.(error_ppformat ~display_format:Dev ~no_colour:false)
         e)
+
+
+module Dummies = struct
+  type t = string * string
+
+  let lst : t list =
+    let loc = Location.generated in
+    [ ( "<TY_EXPR>"
+      , Format.asprintf "%a" PP.ty_expr (t_var ~loc (Ty_variable.of_input_var ~loc "x")) )
+    ; ( "<EXPR>"
+      , Format.asprintf "%a" PP.expr (e_variable ~loc (Variable.of_input_var ~loc "#")) )
+    ; ( "<EXPR1>"
+      , Format.asprintf "%a" PP.expr (e_variable ~loc (Variable.of_input_var ~loc "#1")) )
+    ; ( "<EXPR2>"
+      , Format.asprintf "%a" PP.expr (e_variable ~loc (Variable.of_input_var ~loc "#2")) )
+    ]
+
+
+  let in_output ((dummy, sexp) : t) = dummy, sexp
+  let in_input ((dummy, sexp) : t) = sexp, dummy
+
+  let replace : (t -> t) -> string -> string =
+   fun direction init ->
+    List.fold lst ~init ~f:(fun acc dummy ->
+        let with_, pattern = direction dummy in
+        String.substr_replace_all acc ~pattern ~with_)
+
+
+  let replace_in_input = replace in_input
+  let replace_in_output = replace in_output
+end
 
 let expected_failure_fwd_
     (selector : 'a Pass_type.Selector.t)
@@ -43,20 +75,20 @@ let expected_sucess_fwd_
     (selector : 'a Pass_type.Selector.t)
     (input : 'a)
     (pass : Pass_type.pass)
-    (pp : 'a -> unit)
+    (to_str : 'a -> string)
     : unit
   =
-  pp @@ (selector pass).forward input
+  Format.printf "%s" (to_str @@ (selector pass).forward input)
 
 
 let expected_sucess_bwd_
     (selector : 'a Pass_type.Selector.t)
     (input : 'a)
     (pass : Pass_type.pass)
-    (pp : 'a -> unit)
+    (to_str : 'a -> string)
     : unit
   =
-  pp @@ (selector pass).backward input
+  Format.printf "%s" (to_str @@ (selector pass).backward input)
 
 
 module type S = sig
@@ -64,42 +96,56 @@ module type S = sig
 
   val selector : a Pass_type.Selector.t
   val of_str : string -> a
-  val pp : a -> unit
+  val to_str : a -> string
 end
 
 module Make (X : S) = struct
   let raise : (Errors.t, unit) raise = raise_failwith "test"
-
   let expected_failure_fwd i pass = expected_failure_fwd_ X.selector (X.of_str i) pass
   let expected_failure_bwd i pass = expected_failure_bwd_ X.selector (X.of_str i) pass
-  let expected_sucess_fwd i pass = expected_sucess_fwd_ X.selector (X.of_str i) pass X.pp
-  let expected_sucess_bwd i pass = expected_sucess_bwd_ X.selector (X.of_str i) pass X.pp
+
+  let expected_sucess_fwd i pass =
+    expected_sucess_fwd_ X.selector (X.of_str i) pass X.to_str
+
+
+  let expected_sucess_bwd i pass =
+    expected_sucess_bwd_ X.selector (X.of_str i) pass X.to_str
+
+
   let ( |-> ) = expected_sucess_fwd
   let ( <-| ) = expected_sucess_bwd
   let ( |->! ) = expected_failure_fwd
   let ( !<-| ) = expected_failure_bwd
 end
 
+module Expr = Make (struct
+  type a = expr
+
+  let selector = Pass_type.Selector.expr
+  let of_str = S_exp.expr_of_sexp <@ Sexp.of_string <@ Dummies.replace_in_input
+  let to_str x = Dummies.replace_in_output @@ Format.asprintf "%a" PP.expr x
+end)
+
 module Ty_expr = Make (struct
   type a = ty_expr
 
   let selector = Pass_type.Selector.ty_expr
-  let of_str input = input |> Sexp.of_string |> S_exp.ty_expr_of_sexp
-  let pp x = Format.printf "%a" PP.ty_expr x
+  let of_str = S_exp.ty_expr_of_sexp <@ Sexp.of_string <@ Dummies.replace_in_input
+  let to_str x = Dummies.replace_in_output @@ Format.asprintf "%a" PP.ty_expr x
 end)
 
 module Program = Make (struct
   type a = program
 
   let selector = Pass_type.Selector.program
-  let of_str input = input |> Sexp.of_string |> S_exp.program_of_sexp
-  let pp x = Format.printf "%a" PP.program x
+  let of_str = S_exp.program_of_sexp <@ Sexp.of_string <@ Dummies.replace_in_input
+  let to_str x = Dummies.replace_in_output @@ Format.asprintf "%a" PP.program x
 end)
 
 module Pattern = Make (struct
   type a = pattern
 
   let selector = Pass_type.Selector.pattern
-  let of_str input = input |> Sexp.of_string |> S_exp.pattern_of_sexp
-  let pp x = Format.printf "%a" PP.pattern x
+  let of_str = S_exp.pattern_of_sexp <@ Sexp.of_string <@ Dummies.replace_in_input
+  let to_str x = Dummies.replace_in_output @@ Format.asprintf "%a" PP.pattern x
 end)
--- src/stages/2-ast_unified/PP.ml
+++ src/stages/2-ast_unified/PP.ml
@@ -1,12 +1,5 @@
-open Types
-
-let program ppf (p : program) =
-  Format.fprintf ppf "%a" Sexp.pp_hum (S_exp.sexp_of_program p)
-
-
-let ty_expr ppf (ty : ty_expr) =
-  Format.fprintf ppf "%a" Sexp.pp_hum (S_exp.sexp_of_ty_expr ty)
-
-
-let pattern ppf (p : pattern) =
-  Format.fprintf ppf "%a" Sexp.pp_hum (S_exp.sexp_of_pattern p)
+let pp_as_sexp sexper ppf x = Format.fprintf ppf "%a" Sexp.pp_hum (sexper x)
+let program = pp_as_sexp S_exp.sexp_of_program
+let ty_expr = pp_as_sexp S_exp.sexp_of_ty_expr
+let expr = pp_as_sexp S_exp.sexp_of_expr
+let pattern = pp_as_sexp S_exp.sexp_of_pattern
