dd7cb10812e4d3c8bf9b8f9e45a8e510aae61038
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2023-08-11 20:20:59 +0200

    refactor: rewrite array_to_tuple

--- src/passes/04-nanopasses/passes/array_to_tuple.ml
+++ src/passes/04-nanopasses/passes/array_to_tuple.ml
@@ -1,27 +1,58 @@
 open Ast_unified
 open Pass_type
-(* open Simple_utils.Trace *)
-(* open Errors *)
+open Simple_utils.Trace
+open Errors
 module Location = Simple_utils.Location
 include Flag.No_arg ()
 
-let compile ~raise:_ = Nothing
-  (* let expr : (expr, ty_expr, pattern, _, _) expr_ -> expr = *)
-  (*  fun e -> *)
-  (*   let loc = Location.get_location e in *)
-  (*   match Location.unwrap e with *)
-  (*   | E_array elements -> *)
-  (*     (match elements with *)
-  (*     | [] -> e_unit ~loc *)
-  (*     | hd :: tl -> *)
-  (*       let f = function *)
-  (*         | Array_repr.Expr_entry e -> e *)
-  (*         | Rest_entry e -> raise.error @@ unsupported_rest_property e *)
-  (*       in *)
-  (*       e_tuple ~loc (List.Ne.map f (hd, tl))) *)
-  (*   | e -> make_e ~loc e *)
-  (* in *)
-  (* Fold { idle_fold with expr } *)
+let compile ~raise =
+  let expr : (expr, ty_expr, pattern, _, _) expr_ -> expr =
+   fun e ->
+    let loc = Location.get_location e in
+    match Location.unwrap e with
+    (* | E_array elements -> *)
+    (*   (match elements with *)
+    (*   | [] -> e_unit ~loc *)
+    (*   | hd :: tl -> *)
+    (*     let f = function *)
+    (*       | Array_repr.Expr_entry e -> e *)
+    (*       | Rest_entry e -> raise.error @@ unsupported_rest_property e *)
+    (*     in *)
+    (*     e_tuple ~loc (List.Ne.map f (hd, tl))) *)
+    | E_let_in { lhs = ({ fp = { wrap_content = P_tuple _ ; _ } }, _) as lhs; rhs; body; is_rec; type_params; rhs_type = None } ->
+      let rhs = (match Location.unwrap rhs.fp with
+          | E_array [] -> e_unit ~loc
+          | E_array (hd :: tl) ->
+            let f = function
+              | Array_repr.Expr_entry e -> e
+              | Rest_entry e -> raise.error @@ unsupported_rest_property e
+            in
+            e_tuple ~loc (List.Ne.map f (hd, tl))
+          | _ -> rhs) in
+      make_e ~loc @@ E_let_in { lhs; rhs; body; is_rec; type_params; rhs_type = None }
+    | e -> make_e ~loc e
+  in
+  let declaration : (declaration, expr, ty_expr, pattern, mod_expr, sig_expr) declaration_ -> declaration =
+    fun d ->
+    let loc = Location.get_location d in
+    match Location.unwrap d with
+    | D_multi_const ds ->
+      let f ({ pattern = { fp = { wrap_content = P_tuple _ ; _ } } as pattern; type_params; rhs_type = None; let_rhs } : (pattern, expr, ty_expr) Simple_decl.t) : (pattern, expr, ty_expr) Simple_decl.t =
+        let let_rhs = (match Location.unwrap let_rhs.fp with
+            | E_array [] -> e_unit ~loc
+            | E_array (hd :: tl) ->
+              let f = function
+                | Array_repr.Expr_entry e -> e
+                | Rest_entry e -> raise.error @@ unsupported_rest_property e
+              in
+              e_tuple ~loc (List.Ne.map f (hd, tl))
+            | _ -> let_rhs) in
+        { pattern; type_params; rhs_type = None; let_rhs }
+      in
+      make_d ~loc @@ D_multi_const (List.Ne.map f ds)
+    | d -> make_d ~loc d
+  in
+  Fold { idle_fold with expr ; declaration }
 
 
 let reduction ~raise:_ = Iter.defaults
